b42eeee3d737c91222925a8ba0750e33b0291b50
Abdullah Aslam <aslamabdullah724@gmail.com>
2024-12-20 15:30:13 +0500

    go

--- script.js
+++ script.js
@@ -52,3 +52,13 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('welcomePopup').classList.add('active');
+    }, 1000);
+};
+
+function closePopup() {
+    document.getElementById('welcomePopup').classList.remove('active');
+}
